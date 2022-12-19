eb8bfd48303476290cdb5815989c0c43e12905db
grant chichester <gchichester@gmail.com>
2022-12-19 11:22:54 +0100

    added js to lis

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,38 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+const toggleItem = document.querySelector('#toggleItem');
+const toggleItemOne = document.querySelector('#toggleItemOne');
+const toggleItemTwo = document.querySelector('#toggleItemTwo');
+const about = document.querySelector('.about');
+const projects = document.querySelector('.projects');
+const contact = document.querySelector('.contact');
+
+
+toggleItem.addEventListener('click', () => {
+  console.log("hello");
+ if(about.style.display === 'none') {
+  about.style.display = 'block';
+ } else {
+  about.style.display = 'none';
+ }
+});
+
+toggleItemOne.addEventListener('click', () => {
+  console.log("hello");
+  if(projects.style.display === 'none') {
+    projects.style.display = 'block';
+   } else {
+    projects.style.display = 'none';
+   }
+});
+
+toggleItemTwo.addEventListener('click', () => {
+  console.log("hello");
+  if(contact.style.display === 'none') {
+    contact.style.display = 'block';
+   } else {
+    contact.style.display = 'none';
+   }
+});
