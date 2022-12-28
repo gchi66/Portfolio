f950a03074bb803b7e42b57592058c66c77df2d4
grant chichester <gchichester@gmail.com>
2022-12-28 15:34:52 +0100

    background, disabled highlight on click

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,34 +10,39 @@ const about = document.querySelector('.about');
 const projects = document.querySelector('.projects');
 const contact = document.querySelector('.contact');
 
-toggleItem.addEventListener('click', () => {
-  if(about.style.display === 'none') {
-    about.style.display = 'block';
-    projects.style.display = 'none';
-    contact.style.display = 'none';
-   }
-   else {
-   about.style.display = 'none';
 
-   }
-});
-
-toggleItemOne.addEventListener('click', () => {
-  if(projects.style.display === 'none') {
-    projects.style.display = 'block';
+// Showing the stuff on the right side of the screen when you click the list items
+  toggleItem.addEventListener('click', () => {
+    if(about.style.display === 'none') {
+      about.style.display = 'block';
+      projects.style.display = 'none';
+      contact.style.display = 'none';
+    }
+    else {
     about.style.display = 'none';
-    contact.style.display = 'none';
-   } else {
-    projects.style.display = 'none';
-   }
-});
 
-toggleItemTwo.addEventListener('click', () => {
-  if(contact.style.display === 'none') {
-    contact.style.display = 'block';
-    about.style.display = 'none';
-    projects.style.display = 'none';
-   } else {
-    contact.style.display = 'none';
-   }
-});
+    }
+    event.preventDefault();
+  });
+
+  toggleItemOne.addEventListener('click', () => {
+    if(projects.style.display === 'none') {
+      projects.style.display = 'block';
+      about.style.display = 'none';
+      contact.style.display = 'none';
+    } else {
+      projects.style.display = 'none';
+    }
+    event.preventDefault();
+  });
+
+  toggleItemTwo.addEventListener('click', () => {
+    if(contact.style.display === 'none') {
+      contact.style.display = 'block';
+      about.style.display = 'none';
+      projects.style.display = 'none';
+    } else {
+      contact.style.display = 'none';
+    }
+    event.preventDefault();
+  });
